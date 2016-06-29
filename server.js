6ba030644a4ef9b054d9df8eb5b7e0c92683e9a3
Alan Taylor <alancarltaylor@gmail.com>
2016-06-29 15:20:40 -0600

    expressify commplete

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var api = require('./routes/api');
+
+var app = express();
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1/', api);
+
+app.all('*', function (req,res,next) {
+  res.sendFile('index.html', { root: __dirname + '/public/'})
+})
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
