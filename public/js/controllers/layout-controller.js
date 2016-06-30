b6dcc30a16f5c77e761ab97548089d238ae34424
Alan Taylor <alancarltaylor@gmail.com>
2016-06-30 15:27:27 -0600

    clickable thumbnails

--- public/js/controllers/layout-controller.js
+++ public/js/controllers/layout-controller.js
@@ -13,7 +13,6 @@
   function LayoutController($scope, PlaylistService, $sce){
     $scope.hideNav = true;
     $scope.sortType = "search.json?q=%28and+timestamp%3A1325376000..1328054400+title%3A%27%27%29&sort=top&restrict_sr=on&syntax=cloudsearch"
-
     $scope.getPlaylist = function(val) {
       console.log('CALLED GET PLAYLIST', val);
       PlaylistService.getPlaylist(val)
@@ -23,17 +22,22 @@
           outputArr.push(_.unescape(curr.data.media_embed.content))
           return outputArr;
         }, []);
+        $scope.nowPlaying = $scope.playlist[0]
 
         $scope.thumbnails = videos.data.data.children.reduce(function(outputArr, curr){
           if (curr.data.media){
-            outputArr.push(curr.data.media.oembed.thumbnail_url)
+            curr.data.media.oembed.html = _.unescape(curr.data.media.oembed.html)
+            outputArr.push(curr.data.media.oembed)
           }
             return outputArr;
         }, []);
 
       })
     }
-
+    $scope.switchVideo = function(thumbnail){
+      $scope.nowPlaying = thumbnail.html;
+      console.log("clicked thumbnail: ", thumbnail);
+    }
     $scope.getPlaylist($scope.sortType);
 
     $scope.trustHtml = function(src) {
