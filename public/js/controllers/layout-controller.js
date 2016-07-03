b4710645abc5a45d7e5c065e89f3858a99e7c6af
Alan Taylor <alancarltaylor@gmail.com>
2016-07-02 19:41:50 -0600

    full crud on subs, styling a bit weird

--- public/js/controllers/layout-controller.js
+++ public/js/controllers/layout-controller.js
@@ -13,36 +13,49 @@
 
   function LayoutController($scope, PlaylistService, $sce, $log){
     $scope.domains =  [
-        '5min.com', 'abcnews.go.com', 'animal.discovery.com', 'animoto.com', 'atom.com',
-        'bambuser.com', 'bigthink.com', 'blip.tv', 'break.com',
-        'cbsnews.com', 'cnbc.com', 'cnn.com', 'colbertnation.com', 'collegehumor.com',
-        'comedycentral.com', 'crackle.com', 'dailymotion.com', 'dsc.discovery.com', 'discovery.com',
-        'dotsub.com', 'edition.cnn.com', 'escapistmagazine.com', 'espn.go.com',
-        'fancast.com', 'flickr.com', 'fora.tv', 'foxsports.com',
-        'funnyordie.com', 'gametrailers.com', 'godtube.com', 'howcast.com', 'hulu.com',
-        'justin.tv', 'kinomap.com', 'koldcast.tv', 'liveleak.com', 'livestream.com',
-        'mediamatters.org', 'metacafe.com', 'money.cnn.com',
-        'movies.yahoo.com', 'msnbc.com', 'nfb.ca', 'nzonscreen.com',
-        'overstream.net', 'photobucket.com', 'qik.com', 'redux.com',
-        'revision3.com', 'revver.com', 'schooltube.com',
-        'screencast.com', 'screenr.com', 'sendables.jibjab.com',
-        'spike.com', 'teachertube.com', 'techcrunch.tv', 'ted.com',
-        'thedailyshow.com', 'theonion.com', 'traileraddict.com', 'trailerspy.com',
-        'trutv.com', 'twitvid.com', 'ustream.com', 'viddler.com', 'video.google.com',
-        'video.nationalgeographic.com', 'video.pbs.org', 'video.yahoo.com', 'vids.myspace.com', 'vimeo.com',
-        'wordpress.tv', 'worldstarhiphop.com', 'xtranormal.com',
-        'youtube.com', 'youtu.be', 'zapiks.com'
-        ]
+      '5min.com', 'abcnews.go.com', 'animal.discovery.com', 'animoto.com', 'atom.com',
+      'bambuser.com', 'bigthink.com', 'blip.tv', 'break.com',
+      'cbsnews.com', 'cnbc.com', 'cnn.com', 'colbertnation.com', 'collegehumor.com',
+      'comedycentral.com', 'crackle.com', 'dailymotion.com', 'dsc.discovery.com', 'discovery.com',
+      'dotsub.com', 'edition.cnn.com', 'escapistmagazine.com', 'espn.go.com',
+      'fancast.com', 'flickr.com', 'fora.tv', 'foxsports.com',
+      'funnyordie.com', 'gametrailers.com', 'godtube.com', 'howcast.com', 'hulu.com',
+      'justin.tv', 'kinomap.com', 'koldcast.tv', 'liveleak.com', 'livestream.com',
+      'mediamatters.org', 'metacafe.com', 'money.cnn.com',
+      'movies.yahoo.com', 'msnbc.com', 'nfb.ca', 'nzonscreen.com',
+      'overstream.net', 'photobucket.com', 'qik.com', 'redux.com',
+      'revision3.com', 'revver.com', 'schooltube.com',
+      'screencast.com', 'screenr.com', 'sendables.jibjab.com',
+      'spike.com', 'teachertube.com', 'techcrunch.tv', 'ted.com',
+      'thedailyshow.com', 'theonion.com', 'traileraddict.com', 'trailerspy.com',
+      'trutv.com', 'twitvid.com', 'ustream.com', 'viddler.com', 'video.google.com',
+      'video.nationalgeographic.com', 'video.pbs.org', 'video.yahoo.com', 'vids.myspace.com', 'vimeo.com',
+      'wordpress.tv', 'worldstarhiphop.com', 'xtranormal.com',
+      'youtube.com', 'youtu.be', 'zapiks.com'
+    ]
     $scope.hideNav = true;
     $scope.sortType = "top.json?sort=top&t=week"
     $scope.sub = "/r/videos"
+    $scope.subs = ["politicalvideos", "funny", "buttstuff"]
     $scope.getPlaylist = function(sort, sub) {
+      console.log("sub: ", sub);
       $scope.nowPlaying = null;
       PlaylistService.getPlaylist(sort, sub)
       .then(function(videos) {
 
+        if (videos === "subError"){
+          alert("Big ol Error")
+          $scope.nowPlaying = $scope.thumbnails[0].html
+
+        } else {
+
         $scope.sortType = sort;
         $scope.sub = sub;
+
+        if ($scope.newSub){
+          $scope.subs.push($scope.newSub)
+          $scope.newSub = ""
+        }
 
 
         $scope.thumbnails = videos.data.data.children.reduce(function(outputArr, curr){
@@ -50,14 +63,20 @@
             curr.data.media.oembed.html = _.unescape(curr.data.media.oembed.html)
             outputArr.push(curr.data.media.oembed)
           }
-            return outputArr;
+          return outputArr;
         }, []);
 
         $scope.nowPlaying = $scope.thumbnails[0].html
-        console.log("thumbnails: ", $scope.thumbnails);
+      }
       })
     }
-
+    $scope.removeSub = function(sub){
+      for (var i = 0; i < $scope.subs.length; i++) {
+        if ($scope.subs[i] === sub){
+          $scope.subs.splice(i, 1);
+        }
+      }
+    }
 
     $scope.switchVideo = function(thumbnail){
       $scope.nowPlaying = thumbnail.html;
@@ -84,9 +103,9 @@
     $scope.newSub = "";
     $scope.createSub = function(keyEvent, newSub){
       if (keyEvent.which === 13){
-        $scope.newSub = newSub;
-        $scope.sub = "/r/"+ newSub;
-        $scope.getPlaylist($scope.sortType, $scope.sub);
+        $scope.newSub = newSub
+        $scope.getPlaylist($scope.sortType, "/r/"+$scope.newSub)
+
       }
     }
 
--- public/js/services/playlist-service.js
+++ public/js/services/playlist-service.js
@@ -11,17 +11,23 @@
   function PlaylistService($http){
 
     this.getPlaylist = function(sortType, sub){
-      console.log("sub: ", sub);
-      console.log("sortType: ", sortType);
+
       if (sortType === "new.json" || sortType === "hot.json"){
-        console.log("new sortType, not top");
         return $http.get('https://www.reddit.com'+sub+'/'+sortType);
       } else {
-        return $http.get('https://www.reddit.com'+sub+'/'+sortType+"&limit=100");
-    }
+        return $http.get('https://www.reddit.com'+sub+'/'+sortType+"&limit=100").then(function(data){
+          console.log("successful data: ", data);
+          return data;
+        },
+        function(){
+
+        console.log("error");
+        return "subError";
+        })
+      }
   }
 
-  }
+}
 
 
 
