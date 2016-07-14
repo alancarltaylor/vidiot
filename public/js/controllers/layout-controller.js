022d10716e2a1796ef77e643743a245ccb5f56fb
Alan Taylor <alancarltaylor@gmail.com>
2016-07-13 18:02:26 -0600

    added comments to the code

--- public/js/controllers/layout-controller.js
+++ public/js/controllers/layout-controller.js
@@ -17,6 +17,8 @@
 
   function LayoutController($scope, PlaylistService, $sce, $log, $rootScope, $localStorage) {
 
+    // This array has all of the domains that Vidiot will show videos from.
+
     $scope.domains = [
       '5min.com', 'abcnews.go.com', 'animal.discovery.com', 'animoto.com', 'atom.com',
       'bambuser.com', 'bigthink.com', 'blip.tv', 'break.com',
@@ -38,6 +40,8 @@
       'wordpress.tv', 'worldstarhiphop.com', 'xtranormal.com',
       'youtube.com', 'youtu.be', 'zapiks.com'
     ]
+
+      //This array has all of the known defunct urls
     $scope.defunctUrls = [
       "", "https://i.ytimg.com/vi/fOL20yOKqGg/hqdefault.jpg", 'https://i.ytimg.com/vi/GnjLPXZIkQM/hqdefault.jpg', "https://i.ytimg.com/vi/0hGiW1_aym8/hqdefault.jpg", "https://i.ytimg.com/vi/1e3XOejppak/hqdefault.jpg",
       "https://i.ytimg.com/vi/r2UqT6SZ7CU/hqdefault.jpg", "https://i.ytimg.com/vi/q8eHo5QHlTA/hqdefault.jpg", "https://i.ytimg.com/vi/kODnzx694PQ/hqdefault.jpg", "https://i.ytimg.com/vi/-q69mE40Lz8/hqdefault.jpg",
@@ -52,18 +56,25 @@
 
     $scope.hideNav = true;
     $scope.hoverTitle = "";
+
+    //The below 'embedClass' variable helps with dynamically resizing divs on different devices
     $scope.embedClass = "col s12 m12 l8 push-l2";
+
+    //The default sort is top of the week
     $scope.sortType = "top.json?sort=top&t=week";
+
+    //The player defaults to r/videos
     $scope.sub = "/r/videos";
+
+    //The below array contains the subs that users can add to or delete from. This array is stored on local storage.
     $scope.subs = ["Politicalvideos"];
     $scope.nowPlaying = {};
-    $scope.play = false;
-    $scope.rightKeyPushed = false;
-    $scope.leftKeyPushed = false;
-    $scope.fKeyPushed = false;
-    $scope.spacebarPushed = false;
+    $scope.newSub = "";
+
     $scope.storage = $localStorage;
 
+    //This IIFE runs first to check if there are any subs in local storage, and to set the users subs to that if
+    //there is.
     (function() {
       'use strict';
       if ($scope.storage.subs) {
@@ -74,18 +85,22 @@
 
     }());
 
+    //This function shows the reddit title of each video on the playlist on hover.
     $scope.setHoverTitle = function(hoverTitle) {
       $scope.hoverTitle = hoverTitle;
     }
+    //This function removes the title display once the user is no longer hovering over that video thumbnail
     $scope.removeHoverTitle = function() {
       $scope.hoverTitle = "";
     }
+
+    //This function sets the playlist of videos from reddit, in the sort the user requests, to the 'thumbnails' array
     $scope.getPlaylist = function(sort, sub) {
-      console.log("sub: ", sub);
+
       PlaylistService.getPlaylist(sort, sub)
         .then(function(videos) {
 
-
+          //Show a message if there are no videos
           if (videos === "subError") {
             alert("This sort with this sub has no videos")
             $scope.newSub = "";
@@ -97,12 +112,14 @@
             $scope.sortType = sort;
             $scope.sub = sub;
 
+            //This statement allows users to input their own sub, and get a playlist from it
             if ($scope.newSub) {
               $scope.subs.push($scope.newSub)
               $scope.newSub = ""
             }
 
-
+            //This reduce statement checks if the object has videos, if they are from a domain I trust,
+            //and if they are not from a defunct source. It then pushes them into my 'thumbnails' array.
             $scope.thumbnails = videos.data.data.children.reduce(function(outputArr, curr) {
               if (curr.data.media && ($scope.domains.indexOf(curr.data.domain) !== -1) && ($scope.defunctUrls.indexOf(curr.data.media.oembed.thumbnail_url) === -1)) {
                 curr.data.media.oembed.html = _.unescape(curr.data.media.oembed.html)
@@ -114,7 +131,7 @@
             if ($scope.thumbnails.length === 0) {
               alert("sorry, there aren't any videos with that sort and this sub, try using a different sort")
             } else {
-
+              //This sets the playing video to the first thumbnail in the array
               $scope.nowPlaying.video = $scope.thumbnails[0].html
               $scope.nowPlaying.index = 0;
               $scope.nowPlaying.title = $scope.thumbnails[0].title
@@ -123,6 +140,8 @@
           }
         })
     }
+
+    //This function allows users to remove a sub they don't like
     $scope.removeSub = function(sub) {
       for (var i = 0; i < $scope.subs.length; i++) {
         if ($scope.subs[i] === sub) {
@@ -131,6 +150,7 @@
       }
     }
 
+    //This function allows users to click on a thumbnail, and have it play in the player
     $scope.switchVideo = function(thumbnail) {
       $scope.thumbnails[$scope.nowPlaying.index].active = false;
       $scope.nowPlaying.video = thumbnail.html;
@@ -139,13 +159,15 @@
       $scope.thumbnails[$scope.nowPlaying.index].active = true;
     }
 
+    //Invoking the 'getPlaylist' function, declared above
     $scope.getPlaylist($scope.sortType, $scope.sub);
 
-
+    //This makes sure the iframe is rendered as an iframe, and not as a string
     $scope.trustHtml = function(src) {
       return $sce.trustAsHtml(src);
     }
 
+    //This function hides and shows the nav partial, with different stylings depending on device
     $scope.menuClicked = function() {
       $scope.navClass = "col s12 m12 l4";
       $scope.playerClass = "col s12 m12 l8";
@@ -159,11 +181,8 @@
 
     }
 
-    $scope.displaymessage = function(message) {
-      console.log(message);
-    }
 
-    $scope.newSub = "";
+    //This allows users to create a new sub when they hit enter on the dropdown menu
     $scope.createSub = function(keyEvent, newSub) {
       if (keyEvent.which === 13) {
         $scope.newSub = newSub
@@ -172,6 +191,8 @@
       }
     }
 
+    //This function allows users to navigate the playlist with the left and right arrow keys, or the left
+    //and right arrow buttons on the main view
     $scope.navigate = function(direction, index) {
 
 
@@ -196,6 +217,7 @@
       }
     }
 
+    //This listens for right or left arrow events, and runs the navigate function on them. 
     $scope.$on("right-arrow", function(e, data) {
 
       $scope.navigate("forward", $scope.nowPlaying.index)
